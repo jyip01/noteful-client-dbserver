8783271591c30c23c8e9483475627b327c976dcc
jyip01 <64282396+jyip01@users.noreply.github.com>
2020-08-31 13:08:56 -0400

    Delete 002.do.create_notes_table.sql

--- migrations/002.do.create_notes_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE notes (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name TEXT NOT NULL,
-  modified TIMESTAMP DEFAULT now() NOT NULL,
-  content TEXT NOT NULL,
-  folder_id uuid REFERENCES folders(id) ON DELETE SET NULL
-);
