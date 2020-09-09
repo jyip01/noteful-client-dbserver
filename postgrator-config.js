f413730c576db76780c4eb64ff2fea437fee7c2c
jyip01 <64282396+jyip01@users.noreply.github.com>
2020-09-09 12:47:05 -0400

    Add files via upload

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString":(process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL
+  "role": "jyip",
+  "db": "recommend_test",
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? "postgresql://jyip:1234@localhost/noteful_test"
+    : process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
     
 };
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,10 @@
 const { expect } = require('chai')
 const supertest = require('supertest')
 
+require('dotenv').config()
+
+process.env.TEST_DB_URL = process.env.TEST_DB_URL
+  || "postgresql://jyip:1234@localhost/noteful_test"
+
 global.expect = expect
 global.supertest = supertest
